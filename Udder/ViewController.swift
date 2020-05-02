fed3bea35a555dd0c02a61d22df76d1d25743344
alfrepreci <ap4ed@virginia.edu>
2020-05-02 10:05:55 -0500

    Add view will appear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func updateTextAttributes(conversionHandler: ([NSAttributedString.Key : Any]) -> [NSAttributedString.Key : Any]) {
+        //
+    }
 
 
 }
